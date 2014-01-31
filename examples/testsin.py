Fixed the tests to be more sensible. Added more tests for the GP class. Still some small numerical differences when switching between matrices and ndarrays (the latter tends to provide less impressive results, qualitatively speaking). Most things are operational, only a few areas that use matrix lib are left.

examples/testsin.py
@@ -23,7 +23,7 @@ y += noise
 negLogML = np.inf
 hypInit = None
 nItr = 10
-Q = 6
+Q = 4
 
 # Define core functions
 likFunc = gp.likelihoods.gaussian

kernels.py
@@ -83,7 +83,7 @@ def periodic(hypcov, x=None, z=None, diag=False):
     return K
 
 
-def spectral_mixture(hypcov, x=None, z=None, diag=False):
+def spectral_mixture_mat(hypcov, x=None, z=None, diag=False):
     """
     Spectral Mixture kernel takes the following form [1],
 
@@ -119,10 +119,16 @@ def spectral_mixture(hypcov, x=None, z=None, diag=False):
             d2 = np.zeros((n,n,D))
             for j in np.arange(0,D):
                 d2[:,:,j] = core.sq_dist(x[:,j].T)
+                # d2[:,:,j] = sp.spatial.distance.cdist(np.atleast_2d(x[:,j]).T, 
+                #                                       np.atleast_2d(x[:,j]).T, 
+                #                                       'sqeuclidean')
         else:
             d2 = np.zeros((n,z.shape[0],D))
             for j in np.arange(0,D):
                 d2[:,:,j] = core.sq_dist(x[:,j].T, z[:,j].T)
+                # d2[:,:,j] = sp.spatial.distance.cdist(np.atleast_2d(x[:,j].T), 
+                #                                       np.atleast_2d(z[:,j].T), 
+                #                                       'sqeuclidean')
 
     # Define kernel functions
     k = lambda d2v, dm: np.multiply(np.exp(-2*np.pi**2 * d2v),
@@ -133,7 +139,75 @@ def spectral_mixture(hypcov, x=None, z=None, diag=False):
     for q in range(0,Q):
         C = w[q]**2
         for j in range(0,D):
-            C = C*k(d2[:,:,j]*v[j,q], d[:,:,j]*m[j,q])
+            C = np.dot(C, k(np.dot(d2[:,:,j], v[j,q]), 
+                            np.dot(d[:,:,j], m[j,q])))
         K = K + C
 
+    return K
+
+def spectral_mixture(hypcov, x=None, z=None, diag=False):
+    """
+    Spectral Mixture kernel takes the following form [1],
+
+    k(t) = \sum^Q_{q=0} w_q \prod^P_{p=0} exp(-2pi^2*v^2_{p,q}*t_p^2)
+           * cos(2pi*\mu_{p,q}*t_p)
+
+    It's corresponding hyperparameters are constructed according to
+
+    [ [w_0, w_1, ..., w_q],
+      [mu_0, mu_1, ..., mu_q],
+      [v_0, v_1, ..., v_q] ]
+
+    and then flattened to give hyp = [ w_0, ..., w_q, mu_0, ..., v_q ].
+    So then P is the dimensionality of the data and Q is the number of
+    Gaussians in the Gaussian mixture model (roughly speaking, Q is the
+    number of peaks we attempt to model).
+
+    [1] Wilson, A. G., & Adams, R. P. (2013). Gaussian process covariance
+        kernels for pattern discovery and extrapolation. arXiv preprint 
+        arXiv:1302.4245.
+    
+    """
+    n, D = x.shape
+    hypcov = np.array(hypcov).flatten()
+    Q = hypcov.size/(1+2*D)
+    w = np.exp(hypcov[0:Q])
+    m = np.exp(hypcov[Q+np.arange(0,Q*D)]).reshape(D,Q)
+    v = np.exp(2*hypcov[Q+Q*D+np.arange(0,Q*D)]).reshape(D,Q)
+    d2list = []
+    
+    if 'matrix' in str(type(x)):
+        x = np.asarray(x)
+        if z is not None:
+            z = np.asarray(z)
+    
+    if diag:
+        d2list = [np.zeros((n,1))]*D
+    else:
+        if x is z:
+            d2list = [np.zeros((n,n))]*D
+            for j in np.arange(0,D):
+                xslice = np.atleast_2d(x[:,j]).T
+                d2list[j] = sp.spatial.distance.cdist(xslice, xslice, 'sqeuclidean')
+        else:
+            d2list = [np.zeros((n,z.shape[0]))]*D
+            for j in np.arange(0,D):
+                xslice = np.atleast_2d(x[:,j]).T
+                zslice = np.atleast_2d(z[:,j]).T
+                d2list[j] = sp.spatial.distance.cdist(xslice, zslice, 'sqeuclidean')
+
+    # Define kernel functions
+    k = lambda d2v, dm: np.multiply(np.exp(-2*np.pi**2 * d2v),
+                                    np.cos(2*np.pi * dm))
+    # Calculate correlation matrix
+    K = 0
+    # Need the sqrt
+    dlist = [ np.sqrt(dim) for dim in d2list ]
+    # Now construct the kernel
+    for q in range(0,Q):
+        C = w[q]**2
+        for j,(d,d2) in enumerate(zip(dlist, d2list)):
+            C = np.dot(C, k(np.dot(d2, v[j,q]), 
+                            np.dot(d, m[j,q])))
+        K = K + C
     return K

testing/test_core.py
@@ -14,8 +14,8 @@ import addpath
 import gaussian_process as gp
 
 def test_sqdist():
-    a = np.matrix(np.arange(0,1,0.1)).T
-    b = np.matrix(np.arange(0.5,1.5,0.1)).T
+    a = np.atleast_2d(np.arange(0,1,0.1)).T
+    b = np.atleast_2d(np.arange(0.5,1.5,0.1)).T
     sd_ab = gp.core.sq_dist(a.T,b.T)
     assert np.allclose(sp.spatial.distance.cdist(a,b, 'sqeuclidean'), sd_ab)
     sd_aa = gp.core.sq_dist(a.T,a.T)

testing/test_kernels.py
@@ -18,10 +18,10 @@ class TestKernels:
         f = lambda x: np.sin(2*np.pi*10.*x)
         step = 0.4
         # The spectral mixture kernel doesn't yet work with ndarrays
-        # self.x = np.atleast_2d(np.arange(0,1,step)).T
-        # self.y = np.atleast_2d(f(self.x).ravel()).T
-        self.x = np.matrix(np.arange(0,1,step)).T
-        self.y = np.matrix(f(self.x).ravel()).T
+        self.x = np.atleast_2d(np.arange(0,1,step)).T
+        self.y = np.atleast_2d(f(self.x).ravel()).T
+        # self.x = np.matrix(np.arange(0,1,step)).T
+        # self.y = np.matrix(f(self.x).ravel()).T
 
     def test_rbf(self):
         x = self.x
@@ -46,12 +46,12 @@ class TestKernels:
                           6.615000000000001201934677979330e-37,
                           6.615000000000001201934677979330e-37]])
         diag = np.matrix([0.25]*3).T
-        xxreal =  np.around(gp.kernels.radial_basis(hypcov, x, x, False), 40)
-        xyreal =  np.around(gp.kernels.radial_basis(hypcov, x, y, False), 40)
-        diagreal = np.around(gp.kernels.radial_basis(hypcov, x, None, True), 40)
-        assert (xxreal == xx).all()
-        assert (xyreal == xy).all()
-        assert (diagreal == diag).all()
+        xxreal =  gp.kernels.radial_basis(hypcov, x, x, False)
+        xyreal =  gp.kernels.radial_basis(hypcov, x, y, False)
+        diagreal = gp.kernels.radial_basis(hypcov, x, None, True)
+        assert np.allclose(xxreal, xx)
+        assert np.allclose(xyreal, xy)
+        assert np.allclose(diagreal, diag)
 
     def test_rq(self):
         x = self.x
@@ -64,12 +64,9 @@ class TestKernels:
                        [ 7.38904883,  7.38904883,  7.38904883],
                        [ 7.38902704,  7.38902704,  7.38902704]])
         diag = np.matrix([7.3890561]*3).T
-        assert ( gp.kernels.rational_quadratic(hypcov, x, x, False).all() == 
-                 xx.all() )
-        assert ( gp.kernels.rational_quadratic(hypcov, x, y, False).all() == 
-                 xy.all() )
-        assert ( gp.kernels.rational_quadratic(hypcov, x, None, True).all() == 
-                 diag.all() )
+        assert np.allclose(gp.kernels.rational_quadratic(hypcov, x, x, False), xx)
+        assert np.allclose(gp.kernels.rational_quadratic(hypcov, x, y, False), xy)
+        assert np.allclose(gp.kernels.rational_quadratic(hypcov, x, None, True), diag)
 
     def test_per(self):
         x = self.x
@@ -97,36 +94,26 @@ class TestKernels:
         xxreal = gp.kernels.periodic(hypcov, x, x, False)
         xyreal = gp.kernels.periodic(hypcov, x, y, False)
         diagreal = gp.kernels.periodic(hypcov, x, None, True)
-        assert (xxreal == xx).all()
-        assert (xyreal == xy).all()
-        assert (diagreal == diag).all()
+        assert np.allclose(xxreal, xx)
+        assert np.allclose(xyreal, xy)
+        assert np.allclose(diagreal, diag)
 
     def test_sm(self):
         x = self.x
         y = self.y
-        hypcov = np.array([-0.34657359, -2.7080502, 0.08456656])
-        xx = np.array([[  5.000000002799727116808981008944e-01,
-                          1.170752896709145556009001865050e-02,
-                          1.501742198183448997964587389928e-07],
-                       [  1.170752896709145556009001865050e-02,
-                          5.000000002799727116808981008944e-01,
-                          1.170752896709145556009001865050e-02],
-                       [  1.501742198183448997964587389928e-07,
-                          1.170752896709145556009001865050e-02,
-                          5.000000002799727116808981008944e-01]])
-        xy = np.array([[  5.000000002799727116808981008944e-01,
-                          5.000000002799727116808981008944e-01,
-                          5.000000002799727116808981008944e-01],
-                       [  1.170752896709145556009001865050e-02,
-                          1.170752896709124565854942545684e-02,
-                          1.170752896709102708339145237915e-02],
-                       [  1.501742198183448997964587389928e-07,
-                          1.501742198183392882031831792530e-07,
-                          1.501742198183341795357200517541e-07]])
-        diag = np.matrix([0.500000000279972711680898100894]*3).T
-        xxreal = np.around(gp.kernels.spectral_mixture(hypcov, x, x, False), 16)
+        hypcov = np.array([-0.34657359, -0.34657359, -0.34657359,
+                           -2.7080502, -2.7080502, -2.7080502,
+                            0.08456656, 0.08456656, 0.08456656])
+        xx = np.array([[  1.50000000e+00,   3.51225869e-02,   4.50522659e-07],
+                       [  3.51225869e-02,   1.50000000e+00,   3.51225869e-02],
+                       [  4.50522659e-07,   3.51225869e-02,   1.50000000e+00]])
+        xy = np.array([[  1.50000000e+00,   1.50000000e+00,   1.50000000e+00],
+                       [  3.51225869e-02,   3.51225869e-02,   3.51225869e-02],
+                       [  4.50522659e-07,   4.50522659e-07,   4.50522659e-07]])
+        diag = np.matrix([1.5]*3).T
+        xxreal = gp.kernels.spectral_mixture(hypcov, x, x, False)
         xyreal = gp.kernels.spectral_mixture(hypcov, x, y, False)
         diagreal = gp.kernels.spectral_mixture(hypcov, x, None, True)
-        assert (np.around(xxreal, 16) == np.around(xx, 16)).all()
-        assert (np.around(xyreal, 16) == np.around(xy, 16)).all()
-        assert (np.around(diagreal, 16) == np.around(diag, 16)).all()
+        assert np.allclose(xxreal, xx)
+        assert np.allclose(xyreal, xy)
+        assert np.allclose(diagreal, diag)