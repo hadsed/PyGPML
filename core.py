Important bug fix--somehow, either through a change in atleast_2d() or scipy.spatial.distance.cdist(), the distance matrix calculation was producing an error. Removing a few transposes when defining those distance vectors fixes the problem.

core.py
@@ -60,7 +60,7 @@ def initSMParamsFourier(Q, x, y, sn, samplingFreq, nPeaks, relMaxOrder=2):
 
     # Assign hyperparam length scales (sigma's)
     for i in range(0,D):
-        xslice = np.atleast_2d(x[:,i]).T
+        xslice = np.atleast_2d(x[:,i])
         d2 = spat.distance.cdist(xslice, xslice, 'sqeuclidean')
         if n > 1:
             d2[d2 == 0] = d2[0,1]
@@ -96,7 +96,7 @@ def initSMParams(Q, x, y, sn):
 
     for i in range(0,D):
         # Calculate distances
-        xslice = np.atleast_2d(x[:,i]).T
+        xslice = np.atleast_2d(x[:,i])
         d2 = spat.distance.cdist(xslice, xslice, 'sqeuclidean')
         if n > 1:
             d2[d2 == 0] = d2[0,1]

kernels.py
@@ -119,13 +119,13 @@ def spectral_mixture(hypcov, x=None, z=None, diag=False):
         if x is z:
             d2list = [np.zeros((n,n))]*D
             for j in np.arange(0,D):
-                xslice = np.atleast_2d(x[:,j]).T
+                xslice = np.atleast_2d(x[:,j])
                 d2list[j] = sp.spatial.distance.cdist(xslice, xslice, 'sqeuclidean')
         else:
             d2list = [np.zeros((n,z.shape[0]))]*D
             for j in np.arange(0,D):
-                xslice = np.atleast_2d(x[:,j]).T
-                zslice = np.atleast_2d(z[:,j]).T
+                xslice = np.atleast_2d(x[:,j])
+                zslice = np.atleast_2d(z[:,j])
                 d2list[j] = sp.spatial.distance.cdist(xslice, zslice, 'sqeuclidean')
 
     # Define kernel functions